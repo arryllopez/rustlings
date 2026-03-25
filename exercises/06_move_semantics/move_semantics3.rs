7e82e3cd53461d4a4fd33cd2f3e93e48e1a94043
arryllopez <arryllopez7@gmail.com>
2026-03-25 04:01:31 -0400

    move semantics 3 and 4, some rustlings before bed

--- exercises/06_move_semantics/move_semantics3.rs
+++ exercises/06_move_semantics/move_semantics3.rs
@@ -1,7 +1,9 @@
 // TODO: Fix the compiler error in the function without adding any new line.
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+// the parameters has to be declared as mutable to be able to push new value into the vector, 
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
     vec.push(88);
 
+    // the ownership of vec is moved back to the caller, so the caller can access the modified vector after this function call
     vec
 }
 
@@ -15,8 +17,10 @@ mod tests {
 
     #[test]
     fn move_semantics3() {
+        // immutable vector declared here
         let vec0 = vec![22, 44, 66];
-        let vec1 = fill_vec(vec0);
+        
+        let vec1 = fill_vec(vec0); //the ownership of vec0 is moved to fill_vec, so vec0 is no longer accessible after this line
         assert_eq!(vec1, [22, 44, 66, 88]);
     }
 }
